677024de8c3d403b7c3b5d6071c9ba035a4bbd7c
ikotun-dev <danlogan2003@gmail.com>
2024-09-13 23:38:12 +0100

    added go-ps

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ikotun/portshieldv2
 
 go 1.22.6
+
+require github.com/mitchellh/go-ps v1.0.0 // indirect
---
+++ pkg/ps.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+
+	proc "github.com/mitchellh/go-ps"
+)
+
+func main() {
+	fmt.Println("Portshield V2")
+	var i int
+	fmt.Print("Enter port number : ")
+	fmt.Scan(&i)
+
+	//
+	proccessList, err := proc.Processes()
+	if err != nil {
+		panic(err)
+	}
+
+	// linux command
+	// lsof -i -P | grep -i LISTEN | grep {PID}
+	//
+
+	//check if there a port running
+	// fmt.Print(proccessList)
+	for x := range proccessList {
+		var process proc.Process
+		process = proccessList[x]
+		// log.Printf("PID: %d, Name: %s\n", process.Pid(), process.Executable())
+	}
+
+	fmt.Println("No port like that", i)
+}
